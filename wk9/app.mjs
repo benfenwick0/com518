79f4c8fb5c6cb86fcb92fcf7424b560ec653c2d3
benfenwick0 <5fenwb42@solent.ac.uk>
2024-03-25 10:37:20 +0000

    week9

---
+++ wk9/app.mjs
@@ -0,0 +1,186 @@
+// Solution to the standard exercises for week 2
+
+/*
+ * PLEASE NOTE:
+ *
+ * You can use this code to compare with your lab exercise answer.
+ *
+ * However this code, or code derived from it, MUST NOT be used in the assignment.
+ */
+
+// Import dependencies
+import express from 'express';
+import Database from 'better-sqlite3';
+import expressSession from 'express-session';
+import betterSqlite3Session from 'express-session-better-sqlite3';
+
+// Create our Express server.
+const app = express();
+
+// Enable reading JSON from the request body of POST requests
+app.use(express.json());
+
+// Access static content in the 'public' folder
+app.use(express.static('public'));
+
+// Load the database. You may need to change the path.
+const db = new Database("wadsongs.db");
+const SqliteStore = betterSqlite3Session(expressSession, db);
+
+app.use(expressSession({
+    // Specify the session store to be used.
+    store: new SqliteStore(), 
+
+    // a secret used to digitally sign session cookie, use something unguessable (e.g. random bytes as hex) in a real application.
+    secret: 'BinnieAndClyde', 
+
+    // regenerate session on each request (keeping the session active)
+    resave: true, 
+
+    // save session to store before data is stored in it (disabled as this unnecessarily creates empty sessions)
+    saveUninitialized: false, 
+
+    // reset cookie for every HTTP response. The cookie expiration time will be reset, to 'maxAge' milliseconds beyond the time of the response. 
+    // Thus, the session cookie will expire after 10 mins of *inactivity* (no HTTP request made and consequently no response sent) when 'rolling' is true.
+    // If 'rolling' is false, the session cookie would expire after 10 minutes even if the user was interacting with the site, which would be very
+    // annoying - so true is the sensible setting.
+    rolling: true, 
+
+    // destroy session (remove it from the data store) when it is set to null, deleted etc
+    unset: 'destroy', 
+
+    // useful if using a proxy to access your server, as you will probably be doing in a production environment: this allows the session cookie to pass through the proxy
+    proxy: true, 
+
+    // properties of session cookie
+    cookie: { 
+        maxAge: 600000, // 600000 ms = 10 mins expiry time
+        httpOnly: false // allow client-side code to access the cookie, otherwise it's kept to the HTTP messages
+    }
+}));
+
+
+// Search by artist
+app.get('/artist/:artist', (req, res) => {
+    try {
+        const stmt = db.prepare('SELECT * FROM wadsongs WHERE artist=?');
+        const results = stmt.all(req.params.artist);
+        res.json(results);
+    } catch(error) {
+        res.status(500).json({error: error});
+    }
+});
+
+// Search by title
+app.get('/title/:title', (req, res) => {
+    try {
+        const stmt = db.prepare('SELECT * FROM wadsongs WHERE title=?');
+        const results = stmt.all(req.params.title);
+        res.json(results);
+    } catch(error) {
+        res.status(500).json({error: error});
+    }
+});
+
+// Search by title and artist
+app.get('/artist/:artist/title/:title', (req, res) => {
+    try {
+        const stmt = db.prepare('SELECT * FROM wadsongs WHERE title=? AND artist=?');
+        const results = stmt.all(req.params.title, req.params.artist);
+        res.json(results);
+    } catch(error) {
+        res.status(500).json({error: error});
+    }
+});
+
+// Buy a song with a given ID
+app.post('/song/:id/buy', (req, res) => {
+    try {
+        if(!req.session || !req.session.username){
+            res.status(401).json({error: "Unauthorized: No user logged in"});
+        } else{
+        const stmt = db.prepare('UPDATE wadsongs SET quantity=quantity-1 WHERE id=?');
+        const info = stmt.run(req.params.id);
+        if(info.changes == 1) {
+            res.json({success: 1});
+        } else {
+            res.status(404).json({error: "No song with that ID"});
+        }
+        }
+    } catch(error) {
+        res.status(500).json({error: error});
+    }
+});
+
+// Delete a song with a given ID
+app.delete('/song/:id', (req, res) => {
+    try {
+        if(!req.session || !req.session.username){
+            res.status(401).json({error: "Unauthorized: No user logged in"});
+        } else{
+        const stmt = db.prepare('DELETE FROM wadsongs WHERE id=?');
+        const info = stmt.run(req.params.id);
+        if(info.changes == 1) {
+            res.json({success: 1});
+        } else {
+            res.status(404).json({error: "No song with that ID"});
+        }
+        }
+    } catch(error) {
+        res.status(500).json({error: error});
+    }
+});
+
+// Add a song
+app.post('/song/create', (req, res) => {
+    try {
+        if(req.body.title == "" || req.body.artist == "" || req.body.year == "" || req.body.price == "" || req.body.quantity == "") {
+            res.status(400).json({error: "Blank fields"});
+        } else {
+            const stmt = db.prepare('INSERT INTO wadsongs(title,artist,year,downloads,price,quantity) VALUES(?,?,?,0,?,?)');
+            const info = stmt.run(req.body.title, req.body.artist, req.body.year, req.body.price, req.body.quantity);
+            res.json({id: info.lastInsertRowid});
+        }
+    } catch(error) {
+        res.status(500).json({error: error});
+    }
+});
+
+
+app.use((req,res,next)=>{
+	console.log("Session is");
+	console.log(req.session);
+	next();
+});
+
+//Login POST 
+app.post('/login', (req,res)=>{
+	
+	const stmt = db.prepare('SELECT * FROM ht_users WHERE username=? and password=?')
+	const results = stmt.all(req.body.username, req.body.password);
+	
+	if(results.length == 1){
+		req.session.username = results[0].username;
+		res.json({"username": req.body.username});
+	//if(req.body.username == 'SimonSmith' && req.body.password == 'secret'){
+	//	req.session.username = req.body.username;
+	//	res.json({success: 1)};
+	//}
+	} else{
+		res.status(401).json({error: "Incorrect login!"});
+	}
+	
+});
+
+//Login GET
+app.get('/login', (req,res)=>{
+	res.json({username: req.session.username || null});
+});
+
+//Logout POST route
+app.post('/logout', (req,res)=>{
+	req.session = null;
+	res.json({'success': 1});
+});
+
+app.listen(3000);
---
+++ wk9/public/index.js
@@ -0,0 +1,195 @@
+/*
+ * PLEASE NOTE:
+ *
+ * You can use this code to compare with your lab exercise answer.
+ *
+ * However this code, or code derived from it, MUST NOT be used in the assignment.
+ */
+
+
+
+// Handle button clicks on the search button
+document.getElementById("ht_search").addEventListener("click", e=> {
+
+    // Read in the artist from the form field
+    const artist = document.getElementById("ht_artist").value;
+    ajaxSearch(artist);
+});
+
+async function ajaxSearch(artist) {
+
+    // Send a fetch request to the artist route on this server, passing the
+    // artist as a parameter
+    const ajaxResponse = await fetch(`/artist/${artist}`);
+
+    // Parse the JSON
+    const songs = await ajaxResponse.json();
+
+    // Loop through each song in the JSON
+    let html = "";
+    songs.forEach( song => {
+        // Format the HTML output, using the fields from the current song
+        html += `${song.title} by ${song.artist}, year ${song.year}<br />`;
+		
+		//Create the paragraph
+		let paragraph = document.createElement('p');
+		//Set the innerHTML of the paragraph to the song details
+		//paragraph.innerHTML = `${song.title} by ${song.artist}, year ${song.year}<br />`;
+				
+
+		const textNode = document.createTextNode(`${song.title} by ${song.artist}, year ${song.year}`);
+		
+		
+		//Add the parahraph to the <div>
+		document.getElementById("ht_results").appendChild(paragraph);
+		
+		paragraph.appendChild(textNode);
+		
+		
+		//Create 'buy' button
+		const buttonElement = document.createElement("input");
+		buttonElement.setAttribute("type","button");
+		buttonElement.setAttribute("value", "Buy!");
+		buttonElement.addEventListener ("click", async(e)=>{
+			
+			try{
+				const response = await fetch(`/song/${song.id}/buy`,{
+					method: 'POST'
+				});	
+				
+				if(response.status == 200) {
+					alert("Successfully bought");
+				} else if (response.status == 404) {
+					alert("No song with ID");
+				} else if (response.status == 401) {
+                    alert("User not logged in");
+                } else {
+					alert(`Unknown error: code ${response.status}`);
+				}
+			} 
+			catch(e) {
+				alert(`Error: ${e}`);
+			}
+		
+		});
+		paragraph.appendChild(buttonElement);
+    });
+
+    
+}
+
+
+// Add a song
+document.getElementById("ht_add").addEventListener("click", async() => {
+
+    // Create an object containing the details from the form
+    const song = {
+        "title": document.getElementById("new_title").value,
+        "artist": document.getElementById("new_artist").value,
+        "year": document.getElementById("new_year").value,
+        "quantity": document.getElementById("new_quantity").value,
+        "price": document.getElementById("new_price").value
+    };
+
+    try {
+
+        // Send an AJAX post request to the server, with the song in the body
+        const response = await fetch('/song/create', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(song)
+        });    
+
+        // Handle the status returned from the server
+        if(response.status == 200) {
+            alert("Successfully added");
+        } else if (response.status == 400) {
+            alert("Blank fields");
+        } else {
+            alert(`Unknown error: code ${response.status}`);
+        }
+    } catch(e) {
+        alert(`Error: ${e}`);
+    }
+});
+
+
+
+const loginStatus = async()=>{
+    try{
+        const response = await fetch('/login');
+        const data = await response.json();
+
+        if (data.username == null){//logged in user
+            document.getElementById("buttonlogout").style.display = 'none';
+            document.getElementById("login_form").style.display = 'block';
+            document.getElementById("loggedInMessageFillerText").innerHTML = "";
+        }
+        else{
+            document.getElementById("loggedInMessageFillerText").innerHTML = `Logged in as ${data.username}`;
+            document.getElementById("loggedInMessage").style.display = 'block';
+            document.getElementById("login_form").style.display = 'none';
+            document.getElementById("buttonlogout").style.display = 'block';
+        }
+    } catch (e){
+        alert(`Error: ${e}`);
+    }
+}
+
+
+loginStatus();
+
+
+document.getElementById("loginButton").addEventListener("click", async ()=>{
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    try{
+        const response = await fetch('/login',{
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({username, password})
+        });
+
+        if (response.status ==200){
+            const user = await response.json();
+            document.getElementById("loggedInMessageFillerText").innerHTML = `Logged in as ${user.username}`;
+            document.getElementById("loggedInMessage").style.display = 'block';
+            document.getElementById("login_form").style.display = 'none';
+            document.getElementById("buttonlogout").style.display = 'block';
+        }
+        else{
+            alert('Login failed. Please enter credentials again.')
+        }
+    }catch(error){
+        alert(`Error: ${e}`);
+    }
+
+});
+
+document.getElementById("logoutButton").addEventListener("click", async ()=>{
+    try{
+        const response = await fetch('/logout',{
+            method: 'POST',
+        });
+
+        if (response.status ==200){
+            alert('Logged out successfully');
+            document.getElementById("buttonlogout").style.display = 'none';
+            document.getElementById("login_form").style.display = 'block';
+            document.getElementById("loggedInMessageFillerText").innerHTML = "";
+
+        }
+        else{
+            alert('Login failed. Please enter credentials again.')
+        }
+    }catch(error){
+        alert(`Error: ${e}`);
+    }
+
+});
+
